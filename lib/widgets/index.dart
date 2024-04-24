954c59fad67e402ec355b65164b4b7b004d3c98d
tejaromalius <tayefi.ilia@protonmail.com>
2024-04-24 10:10:19 +0300

    Removed unnecessary widget & updated widgets index

--- lib/widgets/index.dart
+++
@@ -1,4 +0,0 @@
-export 'flotes_logo.dart';
-export 'flotes_title.dart';
-export 'doodle_background.dart';
-export 'google_login_container.dart';
---
+++ lib/widgets/widgets.dart
@@ -0,0 +1,6 @@
+export 'custom_boxshadow.dart' show CustomBoxShadow;
+export 'doodle_background.dart' show DoodleBackground;
+export 'flotes_logo.dart' show FlotesLogo;
+export 'flotes_title.dart' show FlotesTitle;
+export 'google_login_button.dart' show GoogleLoginButton;
+export 'google_login_container.dart' show GoogleLoginButtonWithContainer;
