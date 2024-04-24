a398c7aaacab70105c568f40ece2a6e8cb50659c
tejaromalius <tayefi.ilia@protonmail.com>
2024-04-24 19:02:47 +0300

    Refactored BiometricAuthentication

--- lib/services/biometric_authentication.dart
+++ lib/services/biometric_authentication.dart
@@ -2,50 +2,62 @@ import 'package:get/get.dart';
 import 'package:local_auth/local_auth.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
-SharedPreferences preferences = Get.find<SharedPreferences>();
+enum AuthenticationStatus { waiting, authenticated, failed, canceled }
 
 class BiometricsAuthentication {
   late bool localAuthenticationRequired;
-  late final bool _fingerprintSupported;
+  late final bool fingerprintSupported;
   late final LocalAuthentication _localAuth;
 
   Future<void> init() async {
-    if (!preferences.containsKey("localAuthenticationRequired")) {
-      this.localAuthenticationRequired = false;
-      await preferences.setBool("localAuthenticationRequired", false);
-    } else {
-      this.localAuthenticationRequired =
-          preferences.getBool("localAuthenticationRequired")!;
-    }
-    this._localAuth = LocalAuthentication();
-    this._fingerprintSupported = (await _localAuth.getAvailableBiometrics())
+    _localAuth = LocalAuthentication();
+
+    localAuthenticationRequired =
+        Get.find<SharedPreferences>().getBool("localAuthenticationRequired") ??
+            false;
+
+    fingerprintSupported = (await _localAuth.getAvailableBiometrics())
         .contains(BiometricType.strong);
   }
 
   Future<void> toggle() async {
-    localAuthenticationRequired = !localAuthenticationRequired;
-    await preferences.setBool(
-        "localAuthenticationRequired", localAuthenticationRequired);
+    if (await _scanFingerprint(cancelable: true) ==
+        AuthenticationStatus.authenticated) {
+      localAuthenticationRequired = !localAuthenticationRequired;
+      await Get.find<SharedPreferences>().setBool(
+        "localAuthenticationRequired",
+        localAuthenticationRequired,
+      );
+    }
   }
 
   Future<void> validate() async {
-    if (!this.localAuthenticationRequired || !_fingerprintSupported) return;
+    if (localAuthenticationRequired) await _scanFingerprint();
+  }
 
-    bool biometricsAuthenticated = false;
-    while (!biometricsAuthenticated) {
-      try {
-        biometricsAuthenticated = await _localAuth.authenticate(
-          localizedReason: 'Scan your fingerprint to login',
-          options: AuthenticationOptions(
-            stickyAuth: true,
-            sensitiveTransaction: true,
-            biometricOnly: true,
-            useErrorDialogs: true,
-          ),
-        );
-      } catch (exception) {
-        continue;
-      }
-    }
+  Future<AuthenticationStatus> _scanFingerprint(
+      {bool cancelable = false}) async {
+    AuthenticationStatus authenticationStatus = AuthenticationStatus.waiting;
+
+    do {
+      bool authenticated = await _localAuth.authenticate(
+        localizedReason: 'Scan your fingerprint to validate your identity.',
+        options: AuthenticationOptions(
+          stickyAuth: true,
+          sensitiveTransaction: true,
+          biometricOnly: true,
+          useErrorDialogs: true,
+        ),
+      );
+
+      authenticationStatus = authenticated
+          ? AuthenticationStatus.authenticated
+          : cancelable
+              ? AuthenticationStatus.canceled
+              : AuthenticationStatus.failed;
+      print(authenticationStatus);
+    } while (authenticationStatus != AuthenticationStatus.authenticated &&
+        authenticationStatus != AuthenticationStatus.canceled);
+    return authenticationStatus;
   }
 }
